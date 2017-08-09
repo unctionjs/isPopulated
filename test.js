4ff575e6f7968694efe44c6c9a15197eed360aa0
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-09 13:19:46 -0700

    Making packages actually run and test and then also flow

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type, no-undefined */
 import {test} from "tap"
 
-import isPopulated from "./"
+import isPopulated from "./source.js"
 
 test(({equal, end}) => {
   equal(isPopulated(["a"]), true)
